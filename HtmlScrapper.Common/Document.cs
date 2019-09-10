4dca99ace67d0c4f718b6057c33eeedf0b173901
acuriel <aryan.curiel@gmail.com>
2019-09-10 12:59:36 -0400

    Added Document documentation

--- HtmlScrapper.Common/Document.cs
+++ HtmlScrapper.Common/Document.cs
@@ -8,26 +8,67 @@ using System.Threading.Tasks;
 
 namespace HtmlScrapper.Common
 {
+
+    /// <summary>
+    /// A basic Document
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Document<T> where T:Document<T>
     {
+        /// <summary>
+        /// Build a Document object from a given content using an especific Parser
+        /// </summary>
+        /// <param name="content">The document content</param>
+        /// <param name="parser">The parser to be used</param>
         protected Document(string content, IParser<TagNode> parser)
         {
             Content = content;
             Parser = parser;
             RootTag = parser.Parse(content);
         }
+        /// <summary>
+        /// The RootTag extracted from the docuemnt
+        /// </summary>
         public TagNode RootTag { get; }
+        /// <summary>
+        /// The given content
+        /// </summary>
         public string Content { get; }
+        /// <summary>
+        /// The given Parser
+        /// </summary>
         public IParser<TagNode> Parser { get; }
 
+        /// <summary>
+        /// Start Scrapping the document
+        /// </summary>
         public virtual TagNode Scrap => RootTag;
 
+        /// <summary>
+        /// Load Document from text
+        /// </summary>
+        /// <param name="content">The text content</param>
+        /// <param name="parser">The used parser</param>
+        /// <returns></returns>
         public static Document<T> LoadFromText(string content, IParser<TagNode> parser)
             => new Document<T>(content, parser);
 
+        /// <summary>
+        /// Load document from an especific path
+        /// </summary>
+        /// <param name="path">The path where the content must be extracted</param>
+        /// <param name="parser">The used parser</param>
+        /// <returns></returns>
         public static Document<T> LoadFromPath(string path, IParser<TagNode> parser)
             => LoadFromStream(new FileStream(path, FileMode.Open), parser);
 
+        /// <summary>
+        /// Load Document from a Stream
+        /// </summary>
+        /// <param name="stream">The stream object</param>
+        /// <param name="parser">The used parser</param>
+        /// <param name="encoding">The encoding to be used for reading the stream</param>
+        /// <returns></returns>
         public static Document<T> LoadFromStream(Stream stream, IParser<TagNode> parser, Encoding encoding = null)
         {
             encoding = encoding ?? Encoding.UTF8;
@@ -39,6 +80,13 @@ namespace HtmlScrapper.Common
             return new Document<T>(content, parser);
 
         }
+        /// <summary>
+        /// Async load Document from a Stream
+        /// </summary>
+        /// <param name="stream">The stream object</param>
+        /// <param name="parser">The used parser</param>
+        /// <param name="encoding">The encoding to be used for reading the stream</param>
+        /// <returns></returns>
         public static async Task<Document<T>> LoadFromStreamAsync(Stream stream, IParser<TagNode> parser, Encoding encoding = null)
         {
             encoding = encoding ?? Encoding.UTF8;
@@ -49,6 +97,12 @@ namespace HtmlScrapper.Common
             }
             return new Document<T>(content, parser);
         }
+        /// <summary>
+        /// Load Document from a given url
+        /// </summary>
+        /// <param name="url">The url to extract content</param>
+        /// <param name="parser">The used parser</param>
+        /// <returns></returns>
         public static Document<T> LoadFromUrl(string url, IParser<TagNode> parser)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
@@ -71,6 +125,9 @@ namespace HtmlScrapper.Common
         }
     }
 
+    /// <summary>
+    /// An HTML document
+    /// </summary>
     public class HtmlDocument : Document<HtmlDocument>
     {
         protected HtmlDocument(string content, IParser<TagNode> parser, string docType)
