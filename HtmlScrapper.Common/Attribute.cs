15b31c1367304d7baf285be28791d5f30a2255da
acuriel <aryan.curiel@gmail.com>
2019-09-10 13:00:43 -0400

    Added attribute's documentation

--- HtmlScrapper.Common/Attribute.cs
+++ HtmlScrapper.Common/Attribute.cs
@@ -4,14 +4,29 @@ using System.Text;
 
 namespace HtmlScrapper.Common
 {
+    /// <summary>
+    /// A tag's attribute
+    /// </summary>
     public class Attribute
     {
+        /// <summary>
+        /// Build an attribute with the specific name and value
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
         public Attribute(string name, string value)
         {
             Name = name;
             Value = value;
         }
+
+        /// <summary>
+        /// Attribute's name
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Attribute's value
+        /// </summary>
         public string Value { get; set; }
 
         public override string ToString() => $"{Name}=\"{Value}\"";
